904d9dd0ce8482c4eda216e38a9d0c9d29e46633
HwayoungYoon <54611046+HwayoungYoon@users.noreply.github.com>
2020-06-28 00:10:16 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const u_name = document.querySelector('input[type=text]');
 const wrap = document.getElementById('wrap');
 const tabletMQL = window.matchMedia("all and (min-width: 768px)");
 const pcMQL = window.matchMedia("all and (min-width: 1024px)");
-/* 질문 갯수에 맞게 ENDPOINT 수정 */
 const ENDPOINT = 11;
 const select = [];
 let qIdx = -1;
@@ -149,21 +148,21 @@ const goResult = () => {
     const point = calcScore();
     const grade = sortResult(point);
     const pTitle = document.querySelector('.p');
-    /* const res_point = document.querySelector('.point');
+    const res_point = document.querySelector('.point');
     const pin = document.querySelector('.pin');
     const img_url = 'img/image-' + grade + '.png';
     const res_img = document.createElement('img');
-    const res_img_div = document.querySelector('.art'); */
+    const res_img_div = document.querySelector('.art');
     const major = document.querySelector('.result');
     const desc = document.querySelector('.res');
 
     pTitle.innerHTML = u_name.value + ' 님에게 맞는 전공은';
-    /* res_point.innerHTML = point + '점';
+    res_point.innerHTML = point + '점';
     pin.style.marginLeft = infoList[grade].mLeft;
     res_img.src = img_url;
     res_img.alt = infoList[grade].name;
     res_img.title = infoList[grade].name;
-    res_img_div.appendChild(res_img); */
+    res_img_div.appendChild(res_img);
     major.innerHTML = infoList[grade].name;
     desc.innerHTML = infoList[grade].desc;
 
